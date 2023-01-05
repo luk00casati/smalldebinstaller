6620fe6cb9b46c2b22498c2d7e94bde890375ee3
luk00casati <109902811+luk00casati@users.noreply.github.com>
2023-01-05 14:59:58 +0000

    Add files via upload

--- debinstaller.sh
+++ debinstaller.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #https://github.com/luk00casati/smalldebinstaller.git
-cd ..
+cd
 
-slblk
+lsblk
 
 echo "scegliere il disco su cui installare il sistema"
 
@@ -16,20 +16,20 @@ mkdir -p /mnt/boot/efi #efi
 mount /dev/"$disco"1 /mnt/boot/efi
 apt install debootstrap
 debootstrap testing /mnt
-cp /smalldebinstaller/sources.list /mnt/etc/apt
+cp smalldebinstaller/sources.list /mnt/etc/apt
 for dir in sys dev proc; do mount --rbind /$dir /mnt/$dir && mount --make-rslave /mnt/$dir ; done
 cp /etc/resolv.conf /mnt/etc/
 chroot /mnt /bin/bash
 apt update
 apt install locales
 dpkg-reconfigure locales
-apt install linux-image-amd64 sudo ntp network-manager vim
-cp /smalldebinstaller/fstab /etc/fstab
-cp /smalldebinstaller/hostname /etc/hostname
-cp /smalldebinstaller/hosts /etc/hosts
+apt install linux-image-amd64 linux-firmware sudo ntp network-manager vim
+cp smalldebinstaller/fstab /etc/fstab
+cp smalldebinstaller/hostname /etc/hostname
+cp smalldebinstaller/hosts /etc/hosts
 dpkg-reconfigure tzdata
 apt install grub-efi-amd64
 grub-install /dev/"$disco"
 update-grub
 systemctl enable NetworkManager
-echo 'done may you want make a change before restart es. add user change root password add software change configurations"
+echo "done may you want make a change before restart es. add user change root password add software change configurations"
