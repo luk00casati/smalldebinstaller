87987da9510b5c1373388023d09c0044d418c463
luk00casati <109902811+luk00casati@users.noreply.github.com>
2023-01-06 20:17:55 +0100

    cambio

--- debinstaller.sh
+++ debinstaller.sh
@@ -17,20 +17,21 @@ mount /dev/"$disco"1 /mnt/boot/efi
 apt install debootstrap
 debootstrap testing /mnt
 cp smalldebinstaller/sources.list /mnt/etc/apt
+cp smalldebinstaller /mnt
 for dir in sys dev proc; do mount --rbind /$dir /mnt/$dir && mount --make-rslave /mnt/$dir ; done
 cp /etc/resolv.conf /mnt/etc/
 chroot /mnt /bin/bash <<END
 apt update
-apt install locales
+apt -y install locales
 dpkg-reconfigure locales
-apt install linux-image-amd64 linux-firmware sudo ntp network-manager vim
-cp smalldebinstaller/fstab /etc/fstab
-cp smalldebinstaller/hostname /etc/hostname
-cp smalldebinstaller/hosts /etc/hosts
+apt install linux-image-amd64 firmware-linux sudo ntp network-manager vim
+cp /smalldebinstaller/fstab /etc/fstab
+cp /smalldebinstaller/hostname /etc/hostname
+cp /smalldebinstaller/hosts /etc/hosts
 dpkg-reconfigure tzdata
-apt install grub-efi-amd64
+apt -y install grub-efi-amd64
 grub-install /dev/"$disco"
 update-grub
-systemctl enable NetworkManager
+systemctl enable networking
 echo "done may you want make a change before restart es. add user change root password add software change configurations"
 END
